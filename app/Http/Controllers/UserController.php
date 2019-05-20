d208f9a10151ca33bad6fb76e80a9cfb001f1e5e
Maria <mariak@prdxn.com>
2019-05-20 14:53:46 +0530

    store function completed for user creation in comtroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,38 +8,74 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the user.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return view('user.index', compact('users'));
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new user.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(User $user)
     {
-        //
+        return view('user.create', compact('user'));
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created user in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => ['required', 'max:255'],
+                'email' => ['required', 'email'],
+                'job' => ['nullable', 'string'],
+                'city' => ['nullable', 'string'],
+                'country' => ['nullable', 'string'],
+            ]);
+
+            if ($validated) {
+                $createUser = new User;
+                $createUser = $createUser->createUsers($validated);
+
+                if ($createUser) {
+                    $request->session()->flash('success', 'User created successfully');
+                    return redirect('users');
+                } else {
+                    $request->session()->flash('failure', 'User creation failed');
+                    return redirect()->route('users.create');
+                }
+            } else {
+                return redirect()->route('users.create')->with('error');
+            }
+
+            return redirect('users');
+
+        } catch (\Illuminate\Database\QueryException $e) {
+            $errorCode = $e->errorInfo[1];
+            if ($errorCode == '1062') {
+                return redirect()->back()->with('error', 'Email ID already exist');
+            } else {
+                return redirect()->back()->with('error', 'Database error');
+            }
+        } catch (Exception $e) {
+            return redirect()->back()->with('errors');
+        }
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified user.
      *
      * @param  \App\User  $user
      * @return \Illuminate\Http\Response
@@ -50,18 +86,18 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified user.
      *
      * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
     public function edit(User $user)
     {
-        //
+        return view('user.create', compact('user'));
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified user in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\User  $user
@@ -73,13 +109,15 @@ class UserController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified user from storage.
      *
      * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
     public function destroy(User $user)
     {
-        //
+        $delete = $user->delete();
+
+        return redirect('users');
     }
 }
